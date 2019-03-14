6726cf64abc8a0a306e95d8cfcef0180447806ee
1149472629@qq.com <1149472629@qq.com>
2019-03-14 21:08:46 +0800

    模板lmq

---
+++ RecallOnTimeMVC/Controllers/LmqMVCController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecallOnTimeMVC.Controllers
+{
+    public class LmqMVCController : Controller
+    {
+        #region 影片
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+        #region 场次
+
+        #endregion
+        #region 影厅
+
+        #endregion
+    }
+}
