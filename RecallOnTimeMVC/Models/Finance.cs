060941ce7b82a462562671c10661e85768c474dd
纪伟 薛 <406955883@qq.com>
2019-03-15 20:36:05 +0800

    更改表字段

--- RecallOnTimeMVC/Models/Finance.cs
+++ RecallOnTimeMVC/Models/Finance.cs
@@ -12,9 +12,7 @@ namespace RecallOnTimeMVC.Models
     {
         [Key]
         public int FId { get; set; }//编号
-        public int SessionSId{ get; set; }//场次表外键
-        public string F_Content { get; set; }//活动内容
-        public DateTime F_BeginTime { get; set; }//开始时间
-        public DateTime F_EndTime { get; set; }//结束时间
+        public int Money { get; set; }//金额
+        public DateTime CreateTime { get; set; }//创建时间
     }
 }
